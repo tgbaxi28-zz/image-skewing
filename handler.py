592f7d0b503aff27c83cf39e0a9b49358716cac2
Tanmay Baxi <tgbaxi@Tanmays-MacBook-Air.local>
2020-02-20 09:07:22 +0530

    adding the image skew serverless example

---
+++ handler.py
@@ -0,0 +1,73 @@
+import json
+import boto3
+import cv2
+import numpy as np
+import uuid
+import os
+
+s3Client = boto3.client('s3')
+
+def image_skew(event, context):
+    body = {
+        "message": "Image Skewing Process",
+        "input": event
+    }
+
+    bucketName = event['Records'][0]['s3']['bucket']['name']
+    bucketKey = event['Records'][0]['s3']['object']['name']
+
+    download_path = '/tmp/{}{}'.format(uuid.uuid4(), bucketKey)
+    output_path = '/tmp/{}'.format(bucketKey)
+
+    s3Client.download_file(bucketName, bucketKey, download_path)
+
+    image = cv2.imread(download_path)
+
+    # convert the image to grayscale and flip the foreground
+    # and background to ensure foreground is now "white" and
+    # the background is "black"
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(gray)
+
+    # threshold the image, setting all foreground pixels to
+    # 255 and all background pixels to 0
+    thresh = cv2.threshold(gray, 0, 255,
+        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    # grab the (x, y) coordinates of all pixel values that
+    # are greater than zero, then use these coordinates to
+    # compute a rotated bounding box that contains all
+    # coordinates
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+
+    # the `cv2.minAreaRect` function returns values in the
+    # range [-90, 0); as the rectangle rotates clockwise the
+    # returned angle trends to 0 -- in this special case we
+    # need to add 90 degrees to the angle
+    if angle < -45:
+        angle = -(90 + angle)
+
+    # otherwise, just take the inverse of the angle to make
+    # it positive
+    else:
+        angle = -angle
+
+    # rotate the image to deskew it
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h),
+        flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    # draw the correction angle on the image so we can validate it
+    cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    cv2.imwrite(output_path, rotated)
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
